017316f9f809c5f0613c4f3164a6d567459b41b6
unknown <fvk@ngs.ru>
2015-03-31 17:58:19 +0700

    новый столбец

---
+++ protected/config/database.php
@@ -0,0 +1,11 @@
+<?php
+
+// This is the database connection configuration.
+return array(
+	'connectionString' => 'mysql:host=localhost;dbname=appdb',
+	'emulatePrepare' => true,
+	'username' => 'root',
+	'password' => '',
+	'charset' => 'utf8',
+        'tablePrefix'=>'tbl_',
+);
